3bd420ac0e3fe87cdafa484a4e5077b5558470ae
Mikail Köse <mikail.koese@gmail.com>
2018-10-27 21:39:57 +0200

    Fix duplicate find error

--- fileops.sh
+++ fileops.sh
@@ -1,3 +1,3 @@
 # If duplicate delete both files
-md5sum $(find . -name *.xml -print) | sort | awk '{c[$1]++; f[$1]=f[$1]$2"\n";next;} END{ for (var in c) if(c[var]>1 && f[var]!~/ori/ ) print f[var] }' | sed 's/^\*//' | sed '/^$/d' | xargs rm -f
+md5sum $(find . -name *.xml -print) | awk '{c[$1]++;if(c[$1]==1){t[$1]=$2}else{f1[$1]=t[$1]; f2[$1]=$2;}} END{ for (var in f2) {print f2[var];} for (car in f1){ print f1[car];}}' | sed 's/^*//'
 
